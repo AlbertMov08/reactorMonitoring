e1eef86c8b399110c5d58d0b940312cd2c92d836
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-10-17 15:27:55 -0700

    Tested ZeroShot Model
    Accuracy: 0.3265

--- zeroShotOpenAI.py
+++ zeroShotOpenAI.py
@@ -5,47 +5,36 @@ load_dotenv()
 import os
 from pydantic import BaseModel
 
-# OpenAI API Key
-api_key = os.getenv("OPENAI_API_KEY")
-
-# Function to encode the image
-def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-
-# Path to your image
-image_path = "Foam-mild/frame_0000.jpg"
-
-# Getting the base64 string
-base64_image = encode_image(image_path)
-
-headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {api_key}"
-}
-
-payload = {
-  "model": "gpt-4o-mini",
-  "messages": [
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "Classify this image into the following categories: 'Foam-Heavy', 'Foam-mild', 'Post-Antifoam Addition', 'Foam-Medium', 'No Foam'. Do not provide any explanation of your reasoning. The only output allowed are those aforementioned ones."
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}"
+def zeroShotOpenAIModel(image_path):
+  api_key = os.getenv("OPENAI_API_KEY")
+  def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+      return base64.b64encode(image_file.read()).decode('utf-8')
+  base64_image = encode_image(image_path)
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+  }
+  payload = {
+    "model": "gpt-4o-mini",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "Classify this image into the following categories: 'Foam-Heavy', 'Foam-Mild', 'Post-Antifoam Addition', 'Foam-Medium', 'No Foam'. Do not provide any explanation of your reasoning. The only output allowed are those aforementioned ones."
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": f"data:image/jpeg;base64,{base64_image}"
+            }
           }
-        }
-      ]
-    }
-  ],
-  "max_tokens": 300
-}
-
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
-print(response.json()['choices'][0]['message']['content'])
+        ]
+      }
+    ],
+    "max_tokens": 300
+  }
+  response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+  return (response.json()['choices'][0]['message']['content'])
